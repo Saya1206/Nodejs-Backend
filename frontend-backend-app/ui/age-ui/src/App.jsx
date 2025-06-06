19b286b720805efdd71e72b11767b0db1b45e3ac
Saya1206 <sayali.patil0831>
2025-06-06 22:44:52 +0530

    basic age calculator UI with input and API call

--- frontend-backend-app/ui/age-ui/src/App.jsx
+++ frontend-backend-app/ui/age-ui/src/App.jsx
@@ -1,7 +1,47 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
+
 function App() {
+  const [yearOfBirth, setYearOfBirth] = useState("");
+  const [age, setAge] = useState("");
+
+  useEffect(() => {
+    const getAge = async () => {
+      if (yearOfBirth.length === 4) {
+        try {
+          const res = await axios.get(
+            `http://localhost:5000/age/${yearOfBirth}`
+          );
+          setAge(res.data.age);
+        } catch (err) {
+          console.error("Error fetching age: ", err);
+        }
+      } else {
+        setAge("");
+      }
+    };
+    getAge();
+  }, [yearOfBirth]);
+
   return (
     <>
-      <h1>Age Calculator</h1>
+      <div>
+        <h2>Calculator Age from Year of Birth</h2>
+        <input
+          type="number"
+          placeholder="Enter year of birth"
+          value={yearOfBirth}
+          onChange={(e) => setYearOfBirth(e.target.value)}
+        />
+      </div>
+      <div>
+        {age && (
+          <p>
+            Calculator Age : <strong>{age}</strong>
+          </p>
+        )}
+      </div>
     </>
   );
 }
